99eb38ad85e8189c27bbfc9efee0dae12253a956
Yavor.Nanev <yavor.nanev@digitall.com>
2022-10-25 12:56:19 +0300

    code formating

--- src/com/td/TransitiveDependencies.java
+++ src/com/td/TransitiveDependencies.java
@@ -11,36 +11,39 @@ import java.util.Queue;
 
 public class TransitiveDependencies {
 
-     Map<String, HashSet<String>> inputDependencies = new HashMap<>();
+    Map<String, HashSet<String>> inputDependencies = new HashMap<>();
 
-    public  void computeDependencies(final String testData, final String lineSeparator, final String charSeparator ) {
+    public void computeDependencies(final String testData, final String lineSeparator, final String charSeparator) {
 
-        Arrays.asList(testData.split(lineSeparator)).forEach( line -> extractKeyAndItsDependenciesFromAline(line,charSeparator));
-        inputDependencies.forEach((key,dependencies) -> addTransitiveDependenciesPerKey(dependencies,key));
+        Arrays.asList(testData.split(lineSeparator))
+                .forEach(line -> extractKeyAndItsDependenciesFromAline(line, charSeparator));
+        inputDependencies.forEach((key, dependencies) -> addTransitiveDependenciesPerKey(dependencies, key));
 
     }
 
-    private  void extractKeyAndItsDependenciesFromAline(String key, String charSeparator) {
+    private void extractKeyAndItsDependenciesFromAline(String key, String charSeparator) {
         var inputList = new ArrayList<>(Arrays.asList(key.split(charSeparator)));
-        inputDependencies.put(inputList.get(0), new HashSet<>(inputList.subList(1,inputList.size())));
+        inputDependencies.put(inputList.get(0), new HashSet<>(inputList.subList(1, inputList.size())));
     }
 
     private void addTransitiveDependenciesPerKey(HashSet<String> dependencies, String key) {
 
-       Map<String, HashSet<String>> transitiveDependencies = new HashMap<>();
+        Map<String, HashSet<String>> transitiveDependencies = new HashMap<>();
         Optional.ofNullable(dependencies).ifPresent(depKey ->
-             getDependenciesQueue(depKey).flatMap(this::fetchAllDependencies)
-                             .map(value -> transitiveDependencies.put(key, value)));
+                getDependenciesQueue(depKey).flatMap(this::fetchAllDependencies)
+                        .map(value -> transitiveDependencies.put(key, value)));
 
         System.out.println(transitiveDependencies);
     }
-    private Optional<Queue<String>> getDependenciesQueue(HashSet<String> key){
-       return  Optional.of(new LinkedList<>(key));
+
+    private Optional<Queue<String>> getDependenciesQueue(HashSet<String> key) {
+        return Optional.of(new LinkedList<>(key));
     }
+
     private Optional<HashSet<String>> fetchAllDependencies(Queue<String> dependenciesQueue) {
 
         HashSet<String> allDependencies = new HashSet<>();
-        while (! dependenciesQueue.isEmpty()) {
+        while (!dependenciesQueue.isEmpty()) {
             Queue<String> tempDependenciesQueueQueue = new LinkedList<>();
 
             for (String dep : dependenciesQueue) {
